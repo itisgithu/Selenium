dff84a9dd6599d285a28460155d16026286f0ab8
Nagaraj Naik <168590460+itisgithu@users.noreply.github.com>
2024-11-22 11:50:28 +0530

    Update Assignment.java

--- Seleniumfirstprojcet/src/com/WebDriverDemos/Assignment.java
+++ Seleniumfirstprojcet/src/com/WebDriverDemos/Assignment.java
@@ -3,7 +3,7 @@ package com.WebDriverDemos;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElemen
 
 public class Assignment {
 	
